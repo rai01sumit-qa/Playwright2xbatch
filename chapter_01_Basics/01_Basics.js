e4d218a4109f41e86a77770c280bef6a878f85cd
rai01sumit-qa <rai01sumit@users.noreply.github.com>
2026-05-10 17:58:32 +0530

    Initial commit: JavaScript basics and core concepts

---
+++ chapter_01_Basics/01_Basics.js
@@ -0,0 +1,2 @@
+console.log("Hello The Testing Academy This is Sumit kumar");
+let x = 10;
---
+++ chapter_01_Basics/03_JS_VerifySetup_Commands.js
@@ -0,0 +1,6 @@
+console.log(process.platform);
+// window or linux or mac
+console.log(process.arch);
+//x64
+
+console.log("Node Version:", process.version);
